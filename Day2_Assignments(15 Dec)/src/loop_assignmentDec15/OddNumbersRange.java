7939d35c7ff3ffce79691cd50cfa4d96f211a495
ileana <illeanabledea@gmail.com>
2020-12-17 18:32:55 -0800

    Day 2 Tasks

--- Day2_Assignments(15 Dec)/src/loop_assignmentDec15/OddNumbersRange.java
+++ Day2_Assignments(15 Dec)/src/loop_assignmentDec15/OddNumbersRange.java
@@ -3,32 +3,30 @@ package loop_assignmentDec15;
 import java.util.Scanner;
 
 public class OddNumbersRange {
- 
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello Odd Numbers assignment! ");
-		Range();	
-	
+		Range();
+
 	}
-	
-	public static void  Range() {
+
+	public static void Range() {
 		int a, i;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the first number for range: ");
 		int firstNum = scan.nextInt();
 		System.out.println("Enter the last number for range: ");
 		int lastNum = scan.nextInt();
-		
+
 		scan.close();
-		
-		for(i = firstNum; i<= lastNum; i++) {
+
+		for (i = firstNum; i <= lastNum; i++) {
 			a = i % 2;
-			if(a != 0) {
+			if (a != 0) {
 				System.out.println(i);
 			}
-			
-			
+
 		}
-		
+
 	}
 }
